78640cf1d63769bd3cf0826e318b37861a3b425e
vieiraBeh <brendanascimentovieirah@gmail.com>
2023-12-09 15:49:27 -0400

    commit
    Inserção do Menu e ainda há erros

--- Src/Components/Menu.jsx
+++ Src/Components/Menu.jsx
@@ -1,3 +1,13 @@
+import { Link } from 'react-router-dom'
 export default function Menu(){
+
+    return(
+    <menu className='Menu'>
+
+    <Link to = '/'></Link>
+    <Link to = '/Cadastrar'></Link>
+    <Link to = '/Destaque'></Link>
     
+    </menu>
+    )
 }
