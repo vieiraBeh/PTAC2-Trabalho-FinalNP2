bfbaaee3a1be5822cf117d26e41b6191e59ec331
vieiraBeh <brendanascimentovieirah@gmail.com>
2023-12-09 14:37:33 -0400

    Commit
    Erros no projeto :(

--- Home.jsx
+++ Home.jsx
@@ -1,3 +1,17 @@
-import {Card} from './Components/Card';
-import {Navbar} from './Components/Navbar';
-import {Menu} from './Components/Menu';
+import Card from './Components/Card';
+import Navbar from './Components/Navbar';
+import Menu from './Components/Menu';
+
+export default function Home(){
+
+return(
+<div className = "container">
+    <Menu/>
+    <Navbar nomeSite = {"KPOP-MVS"} />
+    <div className='card-container'>
+        <Card listaMV = {listaMV} />
+    </div>
+</div>
+)
+
+}
--- Src/main.jsx
+++ Src/main.jsx
@@ -1,16 +1,17 @@
+import {Routes,Route, BrowserRouter} from "react-router-dom";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Destaque from './Destaque.jsx';
-import Cadastrar from './Cadastrar.jsx';
-import Home from './Home.jsx';
-import {Routes,Route, BrowserRouter} from "react-router-dom";
+
+import Destaque from './Destaque';
+import Cadastrar from './Cadastrar';
+import Home from './Home';
 import "./Cadastrar.css"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <BrowserRouter>
         <Routes>
-            <Route path = '/' element = {<Home />}></Route>
+            <Route path = '/' element = {<Home/>}></Route>
             <Route path = '/Cadastrar' element = {<Cadastrar />}></Route>
             <Route path = '/Destaque' element = {<Destaque />}></Route>
         </Routes>
